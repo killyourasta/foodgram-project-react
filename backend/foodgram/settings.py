dbbd3b879a12a0ccc3766bd86da5287ebedc9703
Ляйсан Галиева <risatip@yandex.ru>
2023-01-28 20:45:08 +0500

    env

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -18,8 +18,13 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='secret_key')
 DEBUG = True
 
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
-
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+ALLOWED_HOSTS=[
+    '51.250.84.45',
+    'localhost',
+    '127.0.0.1',
+    'foodgramproject.ddns.net',
+]
 
 # Application definition
 
